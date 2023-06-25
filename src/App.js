2a6e60b15645581b5c1e2bac4eef09a0ad8325e7
midas-1119 <lucas.chalie1119@gmail.com>
2023-06-25 14:33:45 -0400

    created/initated homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <img src={logo} className="App-logo img-fluid" alt="logo"/>
+
+        <p>Medical Terminologies doesn't have to be so 😰confusing!</p>
+        <h2>Welcome to your</h2>
+        <h1>Modern Medical Dictionary</h1>
+        
       </header>
     </div>
   );
 }
 
-export default App;
