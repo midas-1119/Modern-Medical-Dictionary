9936a999af5e5fc7643946632710b52ca00e3063
midas-1119 <lucas.chalie1119@gmail.com>
2023-06-27 18:11:27 -0400

    added configjs, commonjs, fxed bugs

---
+++ src/common.js
@@ -0,0 +1,2 @@
+set SECRET_KEY = "xxxxx"
+const SECRET_KEY=proces.env.SECRET_KEY
--- src/dictionary.js
+++ src/dictionary.js
@@ -1,22 +1,26 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./dictionary.css";
 import axios from "axios";
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState(" ");
+  let [keyword, setKeyword] = useState(" ");
+
+  function handleResponse(response) {
+    console.log(response);
+  }
 
   function search(event) {
     event.preventDefault();
-    alert (`Searching for ${keyword} definition`);
-  
-//api code source from Paul (SC)
-let apiKey = `39284b94tea8fb240cc5d3o3a355d5a0`;
-let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=39284b94tea8fb240cc5d3o3a355d5a0`;
-axios.get (apiUrl).then(handleResponse);
-}
+    alert(`Searching for ${keyword} definition`);
+
+    //api code source from Paul (SC)
+    let apiKey = `39284b94tea8fb240cc5d3o3a355d5a0`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key={apiKey}`;
+    axios.get(apiUrl).then(() => handleResponse());
+  }
 
   function handleKeywordChange(event) {
-     setKeyword(event.target.value);
+    setKeyword(event.target.value);
   }
 
   return (
@@ -24,6 +28,6 @@ axios.get (apiUrl).then(handleResponse);
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} autoFocus={true} />
       </form>
-    </div>    
+    </div>
   );
 }
