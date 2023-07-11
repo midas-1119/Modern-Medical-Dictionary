d2e665606a2aa11277e3371ff9d1d5353faf7b9e
midas-1119 <lucas.chalie1119@gmail.com>
2023-07-11 14:20:41 -0400

    Update Dictionary.js

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 
-export default function Dictionary() { 
+export default function Dictionary() {
   let [keyword, setKeyword] = useState(" ");
   let [results, setResults] = useState(null);
 
@@ -12,12 +12,12 @@ export default function Dictionary() {
     //api code source from Paul (SC) + UMLS
     let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then
-    ( response => setResults (response.data))
-  
-  function handleKeywordChange(event) {
-    event.preventDefault();
-    setKeyword(event.target.value);
+    axios.get(apiUrl).then((response) => setResults(response.data));
+
+    function handleKeywordChange(event) {
+      event.preventDefault();
+      setKeyword(event.target.value);
+    }
   }
 
   return (
