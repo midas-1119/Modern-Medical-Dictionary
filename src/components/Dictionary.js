f01b0fb7c766eb00c18cc86a15e6ac8e7661adeb
midas-1119 <lucas.chalie1119@gmail.com>
2023-07-17 13:32:28 -0400

    debug components

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -6,27 +6,28 @@ import Photos from "./Photos";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(" ");
   let [results, setResults] = useState(null);
-
-  function handleScImgResponse(response) {
-    setResults(response.data);
-  }
+  let [resultsPhotos, setResultsPhotos] = useState(null);
 
   function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handleScImgResponse(response) {
+    setResultsPhotos(response.data.photos);
   }
 
   function search(event) {
     event.preventDefault();
 
     //api code source from Paul (SC) + UMLS
-    let apiKey = "4a2e4532802845f5bc50f05be550ae6a";
+    let apiKey = "39284b94tea8fb240cc5d3o3a355d5a0";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleDictionaryResponse);
-  }
 
-  const shecodesImgApikey = "39284b94tea8fb240cc5d3o3a355d5a0";
-  let shecodesImgUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${shecodesImgApikey}`;
-  axios.get(shecodesImgUrl).then(handleScImgResponse);
+    const shecodesImgApikey = "39284b94tea8fb240cc5d3o3a355d5a0";
+    let shecodesImgUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${shecodesImgApikey}`;
+    axios.get(shecodesImgUrl).then(handleScImgResponse);
+  }
 
   function handleKeywordChange(event) {
     event.preventDefault();
@@ -44,8 +45,8 @@ export default function Dictionary() {
           />
         </form>
       </section>
-      {results && <Results results={results} />}
-      {<Photos photos={Photos} />}
+      <Results results={results} />
+      <Photos photos={resultsPhotos} />
     </div>
   );
 }
